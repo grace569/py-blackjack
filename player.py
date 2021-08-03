4f68a0365f29b68ce7865bde8f121fc21a4171df
Grace X Wang <grace.x.wang@hk.pwc.com>
2021-08-03 19:02:07 +0800

    add bet methods

--- player.py
+++ player.py
@@ -1,6 +1,18 @@
+from card import Card
 
 
 class Player:
     def __init__(self, name):
         self.name = name
         self.hand = []
+        self.chips = 100
+        self.bet = 0
+
+    def place_bet(self, amount):
+        pass
+
+    def win_bet(self, amount):
+        pass
+
+    def lose_bet(self, amount):
+        pass
